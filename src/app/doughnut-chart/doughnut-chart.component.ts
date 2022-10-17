2c12c6e2433e18a6b16d3d9e2e1735bb8ac89f7f
Ed O'Connor <ed@oconnor.app>
2022-10-17 15:44:03 -0400

    html template edits

--- src/app/doughnut-chart/doughnut-chart.component.ts
+++ src/app/doughnut-chart/doughnut-chart.component.ts
@@ -13,10 +13,11 @@ export class DoughnutChartComponent {
     labels: this.doughnutChartLabels,
     datasets: [
       { data: [ 350, 450, 100 ] },
-      { data: [ 50, 150, 120 ] },
+      // { data: [ 50, 150, 120 ] },
       { data: [ 250, 130, 70 ] }
     ]
   };
+
   public doughnutChartType: ChartType = 'doughnut';
 
   // events
