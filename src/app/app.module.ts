62fda46ed355bcd5f5edcf010981c8adccbda97c
Ed O'Connor <ed@oconnor.app>
2022-10-17 16:21:53 -0400

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,9 +11,16 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { NgChartsModule } from 'ng2-charts';
 import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent, DoughnutChartComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DoughnutChartComponent,
+    PageNotFoundComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -23,9 +30,10 @@ import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.componen
     MatIconModule,
     NgChartsModule,
     RouterModule.forRoot([
-      {path: './home', component: HomeComponent},
-      {path: './doughnut-chart', component: DoughnutChartComponent},
+      { path: 'home', component: HomeComponent },
+      { path: 'doughnut-chart', component: DoughnutChartComponent },
       { path: '', redirectTo: './home', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent },
     ]),
   ],
   providers: [],
